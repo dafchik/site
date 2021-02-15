98aa9e8ddf7a1152b1a16d603026da9974ff43c4
dafchik <79081864+dafchik@users.noreply.github.com>
2021-02-15 16:25:21 +0700

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.menu-burger-header').click(function() {
+        $('.menu-burger-header').toggleClass('open-menu');
+        $('.mobile-menu').toggleClass('open-menu');
+    });
+});
